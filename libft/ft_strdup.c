0251306e05e1c200f0424ea79b75777a25622eff
42jeolim <ljh2022.tmp@gmail.com>
2022-07-06 17:02:35 +0900

    fixed(header)

--- libft/ft_strdup.c
+++ libft/ft_strdup.c
@@ -6,11 +6,11 @@
 /*   By: jeolim <jeolim@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/06 16:57:26 by jeolim            #+#    #+#             */
-/*   Updated: 2022/07/06 16:58:18 by jeolim           ###   ########.fr       */
+/*   Updated: 2022/07/06 17:01:33 by jeolim           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stdlibh.h>
+#include <stdlib.h>
 char* ft_strdup(const char *s1)
 {
     
