e43c52e2b15bfd6a31e69aa973c9d694ff17a88f
LimJeonghyun <22000617@handong.edu>
2022-07-11 22:32:13 +0900

    fixed(correctly)

--- libft/ft_calloc.c
+++ libft/ft_calloc.c
@@ -19,7 +19,7 @@ void	*ft_calloc(size_t count, size_t size)
 	ptr = malloc(size * count);
 	if (!ptr)
 		return (NULL);
-	if (count >= SIZE_MAX || size >= SIZE_MAX || size * count >= SIZE_MAX)
+	if (!count && (count >= SIZE_MAX || size >= SIZE_MAX || size >= SIZE_MAX / count))
 		return (NULL);
 	return (ft_memset(ptr, 0, (size * count)));
 }
