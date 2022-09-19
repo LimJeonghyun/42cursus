f79d9abbab151dea7e6fbbc4b9052b7d7274df6d
42jeolim <ljh2022.tmp@gmail.com>
2022-09-19 20:27:03 +0900

    gnl_bonus

--- gnl_bonus/gnl.c
+++ gnl_bonus/gnl.c
@@ -6,7 +6,19 @@
 /*   By: jeolim <jeolim@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/19 19:58:26 by jeolim            #+#    #+#             */
-/*   Updated: 2022/09/19 20:02:17 by jeolim           ###   ########.fr       */
+/*   Updated: 2022/09/19 20:26:02 by jeolim           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <gnl.h>
+
+char *get_next_line(int fd)
+{
+	static s_node	buffer;
+	char 			*line;
+
+	if (fd < 0 || BUFFER_SIZE <= 0 || read(fd, 0, 0) < 0)
+		return (NULL);
+
+	return (line);
+}
